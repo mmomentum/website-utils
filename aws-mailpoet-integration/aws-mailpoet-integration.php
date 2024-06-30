1a85c5c7bf8d1415feb193c9189078e6ace5d544
mmomentum <lese.audio@gmail.com>
2024-06-30 02:06:09 -0400

    Create aws-mailpoet-integration.php

---
+++ aws-mailpoet-integration/aws-mailpoet-integration.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Plugin Name: AWS SNS to MailPoet
+ * Description: Handles AWS SNS notifications for SES and updates MailPoet user status, because Mailpoet's SES sending action doesn't cover bounce & complaint handling
+ * Version: 1.0
+ * Author: Aidan Baker
+ * Author URI: https://lese.io
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+class AWSSNSMailPoet {
+    public function __construct() {
+        add_action( 'rest_api_init', array( $this, 'register_endpoints' ) );
+    }
+
+    public function register_endpoints() {
+        register_rest_route( 'aws-sns/v1', '/notification', array(
+            'methods' => 'POST',
+            'callback' => array( $this, 'handle_sns_notification' ),
+        ));
+    }
+
+    public function handle_sns_notification( $request ) 
+	{
+        $body = json_decode( $request->get_body(), true );
+
+        if ( isset( $body['Type'] ) && $body['Type'] == 'SubscriptionConfirmation' ) 
+		{
+            $this->confirm_subscription( $body['SubscribeURL'] );
+            return new WP_REST_Response( 'Subscription confirmed', 200 );
+        }
+
+        if ( isset( $body['Type'] ) && $body['Type'] == 'Notification' )
+		{
+            $message = json_decode( $body['Message'], true );
+
+            if ( isset( $message['notificationType'] ) ) 
+			{
+                $email = '';
+
+                switch ( $message['notificationType'] ) 
+				{
+                    case 'Bounce':
+                        $email = $message['bounce']['bouncedRecipients'][0]['emailAddress'];
+                        $this->update_mailpoet_status( $email, 'bounced' );
+                        break;
+
+                    case 'Complaint':
+                        $email = $message['complaint']['complainedRecipients'][0]['emailAddress'];
+                        $this->update_mailpoet_status( $email, 'complaint' );
+                        break;
+                }
+            }
+        }
+
+        return new WP_REST_Response( 'Notification handled', 200 );
+    }
+
+    private function confirm_subscription( $subscribe_url ) {
+        $response = wp_remote_get( $subscribe_url );
+
+        if ( is_wp_error( $response ) ) {
+            error_log( 'AWS SNS subscription confirmation failed: ' . $response->get_error_message() );
+        } else {
+            error_log( 'AWS SNS subscription confirmed: ' . $subscribe_url );
+        }
+    }
+
+    private function update_mailpoet_status( $email, $status ) 
+	{
+		$mailpoet_api = \MailPoet\API\API::MP('v1');
+
+        if ( $subscriber ) 
+		{
+            if ( $status == 'bounced' ) 
+			{
+                $mailpoet_api->unsubscribe($email);
+            } 
+			elseif ( $status == 'complaint' ) 
+			{
+                $mailpoet_api->unsubscribe($email);
+            }
+        }
+    }
+}
+
+new AWSSNSMailPoet();
