6d89587e82d59086a04aa053d2d6546d61e489c0
mmomentum <lese.audio@gmail.com>
2024-10-12 05:21:47 -0400

    Update bulk-edd-licenses.php
    fixed

--- bulk-edd-licenses/bulk-edd-licenses.php
+++ bulk-edd-licenses/bulk-edd-licenses.php
@@ -2,99 +2,109 @@
 /**
  * Plugin Name: Bulk EDD License Keys
  * Description: Batch License Generator
- * Version: 1.0
+ * Version: 1.2
  * Author: Aidan Baker
  * Author URI: https://lese.io
  */
 
-// generate an N sized alphanumeric license key to use (the chances of collision are astronomical at higher lengths)
-function generate_key($length)
-{
-    $characters = 'abcdef0123456789';
-    $string = '';
+// Ensure the file is not accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
 
-    $max = strlen($characters) - 1;
-    for ($i = 0; $i < $length; $i++) {
-        $string .= $characters[mt_rand(0, $max)];
+// Add admin menu item
+add_action('admin_menu', 'edd_bulk_license_menu');
+function edd_bulk_license_menu() {
+    add_submenu_page(
+        'edit.php?post_type=download',  // Parent slug (EDD main menu)
+        'Bulk License Key Generator',   // Page title
+        'Bulk License Keys',            // Menu title
+        'manage_options',               // Capability
+        'edd-bulk-license-generator',   // Menu slug
+        'edd_bulk_license_generator_page'  // Callback function
+    );
+}
+
+// Display form in the admin page
+function edd_bulk_license_generator_page() {
+    ?>
+    <div class="wrap">
+        <h1>Bulk License Key Generator</h1>
+        <form method="post" action="">
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row">Key Length</th>
+                    <td><input type="number" name="key_length" value="20" min="5" required /></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">Number of Keys</th>
+                    <td><input type="number" name="num_keys" value="10" min="1" required /></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">Product</th>
+                    <td>
+                        <select name="product_id" required>
+                            <?php
+                            // Fetch all EDD products
+                            $products = get_posts(array(
+                                'post_type' => 'download',
+                                'numberposts' => -1
+                            ));
+                            foreach ($products as $product) {
+                                echo '<option value="' . $product->ID . '">' . $product->post_title . '</option>';
+                            }
+                            ?>
+                        </select>
+                    </td>
+                </tr>
+            </table>
+            <p class="submit"><input type="submit" name="generate_keys" class="button-primary" value="Generate License Keys" /></p>
+        </form>
+    </div>
+    <?php
+
+    // Handle form submission
+    if (isset($_POST['generate_keys'])) {
+        $key_length = intval($_POST['key_length']);
+        $num_keys = intval($_POST['num_keys']);
+        $product_id = intval($_POST['product_id']);
+
+        if ($key_length > 0 && $num_keys > 0 && $product_id > 0) {
+            edd_generate_license_keys($key_length, $num_keys, $product_id);
+        }
     }
-
-    return $string;
 }
 
-add_action('admin_menu', 'license_creator_menu');
+// Function to generate random keys with lowercase alphanumeric characters
+function edd_generate_license_keys($key_length, $num_keys, $product_id) {
+    global $wpdb;
 
-// Add a menu item for the plugin
-function license_creator_menu()
-{
-    add_submenu_page('edit.php?post_type=download', 'Bulk EDD License Keys', 'Bulk Licenses', 'manage_options', 'bulk-edd-licenses', 'license_creator_page');
-}
+    $characters = '0123456789abcdef';  // Only lowercase alphanumeric characters
 
-function license_creator_page()
-{
-    if (isset($_POST['license_gen'])) {
-        global $wpdb;
-
-        $number_of_licenses = $_POST['num_licenses'];
-
-        $num_characters = $_POST['num_characters'];
-
-        for ($i = 1; $i <= $number_of_licenses; $i++) {
-            $license_key = generate_key($num_characters);
-
-            $data = array(
-                'license_key' => $license_key,
-                'status' => "inactive",
-                'download_id' => $_POST['download_id'][0],
-                'payment_id' => -1,
-                'cart_index' => $i,
-                'expiration' => 0,
-                'parent' => 0,
-                'customer_id' => -1,
-                'user_id' => -1,
-            );
-
-            $wpdb->insert('wp_edd_licenses', $data);
+    for ($i = 0; $i < $num_keys; $i++) {
+        // Generate random license key
+        $license_key = '';
+        for ($j = 0; $j < $key_length; $j++) {
+            $license_key .= $characters[mt_rand(0, strlen($characters) - 1)];
         }
 
-        echo '<div class="notice notice-success is-dismissible"><p>Licenses Created. Check the database.</p></div>';
-    } ?>
-	
-<div class="wrap">
-<h1>Batch License Creator</h1>
-<p>Creates batch licenses with no order associations</p>
-    <form method="post" action="">
-        <table class="form-table">
-            <tr valign="top">
-                <th scope="row">Number of Licenses</th>
-                <td><input type="number" name="num_licenses" min="1" max="1000" required></td>
-            </tr>
-			<tr valign="top">
-                <th scope="row">License Complexity</th>
-                <td><input type="number" name="num_characters" min="8" max="32" value="16" required></td>
-            </tr>
-			
-            <tr valign="top">
-                <th scope="row">License Key Product</th>
-                <td>
-                    <?php
-                    $downloads = get_posts(array(
-                        'post_type' => 'download',
-                        'post_status' => 'publish',
-                        'posts_per_page' => -1,
-                    ));
-                    foreach ($downloads as $download) {
-                        echo '<label>';
-                        echo '<input type="checkbox" name="download_id[]" value="' . $download->ID . '"> ';
-                        echo $download->post_title;
-                        echo '</label><br>';
-                    }
-                    ?>
-                </td>
-            </tr>
-        </table>
+        // Insert into the EDD license keys table from EDD Software Licensing add-on
+        $wpdb->insert(
+            $wpdb->prefix . 'edd_licenses',  // EDD License table (for Software Licensing add-on)
+            array(
+                'license_key'        => $license_key,
+                'download_id' => $product_id,
+                'status'     => 'inactive',  // default status
+                'date_created' => current_time('mysql')
+            ),
+            array(
+                '%s',
+                '%d',
+                '%s',
+                '%s'
+            )
+        );
+    }
 
-        <?php submit_button('Generate Discount Codes', 'primary', 'license_gen'); ?>
-    </form>
-</div>
-	<?php
+    echo '<div class="notice notice-success"><p>' . $num_keys . ' license keys generated for product ID ' . $product_id . '.</p></div>';
 }
