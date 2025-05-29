c8b758d079b7b9bb248cc76b69d501137dff4627
mmomentum <lese.audio@gmail.com>
2025-05-29 04:46:45 -0400

    Update recent-post-rest.php
    now a post requires a "notify" tag to be returned

--- recent-post-rest/recent-post-rest.php
+++ recent-post-rest/recent-post-rest.php
@@ -15,39 +15,47 @@ if (!defined('ABSPATH')) {
 add_action('rest_api_init', function () {
     register_rest_route('recent-post-api/v1', '/latest-post/', array(
         'methods' => 'GET',
-        'callback' => 'get_most_recent_post',
+        'callback' => 'get_most_recent_notify_or_latest_post',
         'permission_callback' => '__return_true', // Allow access to all users
     ));
 });
 
-// Callback function to return the most recent post
-function get_most_recent_post() {
-    // Get the most recent post
-    $args = array(
-        'numberposts' => 1, // Only one post
-        'post_status' => 'publish', // Only published posts
+// Callback function to return the most recent "notify"-tagged post, or fallback
+function get_most_recent_notify_or_latest_post() {
+    // First, try to get the most recent post with the "notify" tag
+    $args_with_notify = array(
+        'numberposts' => 1,
+        'post_status' => 'publish',
+        'tag' => 'notify',
     );
-    
-    $recent_posts = wp_get_recent_posts($args);
-    
-    // If no posts found, return an error
-    if (empty($recent_posts)) {
+
+    $posts = wp_get_recent_posts($args_with_notify);
+
+    // If no post with "notify" tag found, get the most recent post
+    if (empty($posts)) {
+        $args_fallback = array(
+            'numberposts' => 1,
+            'post_status' => 'publish',
+        );
+        $posts = wp_get_recent_posts($args_fallback);
+    }
+
+    // Still no post found
+    if (empty($posts)) {
         return new WP_Error('no_posts', 'No posts found', array('status' => 404));
     }
 
-    // Get the first (and only) post
-    $post = $recent_posts[0];
-    
-    // Prepare the response data
+    $post = $posts[0];
+
     $response = array(
         'id' => $post['ID'],
         'title' => $post['post_title'],
         'content' => $post['post_content'],
-        'excerpt' => wp_trim_words($post['post_content'], 40, '...'), // Generate the excerpt
+        'excerpt' => wp_trim_words($post['post_content'], 40, '...'),
         'date' => $post['post_date'],
         'author' => get_the_author_meta('display_name', $post['post_author']),
         'link' => get_permalink($post['ID']),
-        'featured_image' => get_the_post_thumbnail_url($post['ID'], 'medium'), // Get scaled image (medium size)
+        'featured_image' => get_the_post_thumbnail_url($post['ID'], 'medium'),
     );
 
     return rest_ensure_response($response);
